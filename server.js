01944fa80829f6a82b7a359ef3ea2228ac601246
mkhan42 <michallekhan9@gmail.com>
2022-06-07 10:20:12 -0400

    imported dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
+
